c8f3db1ebe7928d643c385c32dba2d460c7c12dd
logarithm <iamrohan04@gmail.com>
2018-12-15 17:23:50 +0530

    schema completed

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
 
 const app = express();
 const port = 8080 || process.env.PORT;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
 // Importing all the routes/api
 const auth = require("./routes/api/auth");
@@ -15,6 +20,20 @@ const question = require("./routes/api/question");
 app.use("/api/auth", auth);
 app.use("/api/questions", question);
 app.use("/api/profile", profile);
+
+var db = require("./setup/DBSetup").DBURL;
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 //Basic Checking of Server
 app.get("/", (req, res) => {
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const bodyparser = require("body-parser");
+
+router.use(bodyparser.urlencoded({ extended: false }));
+router.use(bodyparser.json());
+
+const Person = require("./../../models/Person");
 
 router.get("/", (req, res) => {
   res.json({ sucess: true });
+});
+
+router.post("/register", (req, res) => {
+  Person.findOne({ email: req.body.email })
+    .then()
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
